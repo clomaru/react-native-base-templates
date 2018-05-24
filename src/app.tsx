202b1104594a97852707cfb2b75ab4eb258bba22
clomaru <k.cloudspider@gmail.com>
2018-05-24 22:53:16 +0900

    [Add]addButton

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { css } from 'styled-components';
 import styled from 'styled-components/native';
-import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
+import {
+	Platform,
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	TouchableOpacity
+} from 'react-native';
 
 const Container = styled.View`
 	flex: 1;
@@ -10,31 +17,60 @@ const Container = styled.View`
 
 const MyTextInput = styled.TextInput`
 	margin-top: 30px;
-	background-color: grey;
+	background-color: #eee;
 	padding: 10px;
+`;
+
+const AddButton = styled.TouchableOpacity`
+	background-color: #333;
+	padding: 14px;
+	border-radius: 4px;
+	margin-top: 10px;
+`;
+
+const AddButtonText = styled.Text`
+	color: #fff;
+	text-align: center;
+	font-weight: bold;
 `;
 
 export interface Props {}
 
-export default class App extends React.Component<Props, {}> {
+export default class App extends React.Component<Props, { value: string }> {
 	constructor(props: Props) {
 		super(props);
 		this.state = {
-			newTodo: ''
+			newTodo: '',
+			todos: []
 		};
 	}
 
-	render(): JSX.Element {
+	public render(): JSX.Element {
+		console.log(this.state);
 		return (
 			<Container>
 				<Text>text form</Text>
-				<MyTextInput onChangeText={text => this.onChangetext(text)} />
+				<MyTextInput
+					value={this.state.newTodo}
+					onChangeText={text => this.onChangetext(text)}
+				/>
+				<AddButton onPress={() => this.onPressAdd()}>
+					<AddButtonText>Add</AddButtonText>
+				</AddButton>
 			</Container>
 		);
 	}
 
-	private onChangetext(text: string) {
+	private onChangetext(text: string): void {
 		console.log(text);
 		this.setState({ newTodo: text });
+	}
+
+	private onPressAdd(): void {
+		const { newTodo } = this.state;
+		this.setState({
+			newTodo: '',
+			todos: [newTodo, ...this.state.todos]
+		});
 	}
 }
