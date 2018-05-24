c95de3b812d2a12c34b1351051db4e36c130a7ad
clomaru <k.cloudspider@gmail.com>
2018-05-24 20:05:49 +0900

    [Add]add typescript

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { css } from 'styled-components';
 import styled from 'styled-components/native';
 import { Platform, StyleSheet, Text, View } from 'react-native';
@@ -9,18 +9,6 @@ const instructions = Platform.select({
 		'Double tap R on your keyboard to reload,\n' +
 		'Shake or press menu button for dev menu'
 });
-
-export default class App extends Component<{}> {
-	render() {
-		return (
-			<Container>
-				<WelcomeText>Welcome to React Native!</WelcomeText>
-				<InstructionsText>To get started, edit App.js</InstructionsText>
-				<InstructionsText>{instructions}</InstructionsText>
-			</Container>
-		);
-	}
-}
 
 const Container = styled.View`
 	background-color: #f5fcff;
@@ -40,3 +28,15 @@ const InstructionsText = styled.Text`
 	color: #333333;
 	margin-bottom: 5px;
 `;
+
+export default class App extends React.Component<{}> {
+	render() {
+		return (
+			<Container>
+				<WelcomeText>Add TypeScript</WelcomeText>
+				<InstructionsText>To get started, edit App.js</InstructionsText>
+				<InstructionsText>{instructions}</InstructionsText>
+			</Container>
+		);
+	}
+}
