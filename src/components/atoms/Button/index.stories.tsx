de3c4a6ca1538fd762ccacb4129933331c754eb0
clomaru <k.cloudspider@gmail.com>
2018-05-30 03:21:09 +0900

    [Add]stories

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -1,7 +1,5 @@
-// import React from 'react';
-// import Button, { PrimaryButton, WarningButton } from './index.js';
-//
-// export default stories => stories
-//   .add('デフォルト', () => <Button>デフォルト</Button>)
-//   .add('プライマリ', () => <PrimaryButton>プライマリ</PrimaryButton>)
-//   .add('警告', () => <WarningButton>警告</WarningButton>);
+import * as React from 'react';
+import Button from './index.js';
+
+export default stories =>
+	stories.add('デフォルト', () => <Button>デフォルト</Button>);
---
+++ src/components/atoms/ListItem/index.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import ListItem from './index.js';
+
+export default stories =>
+	stories.add('デフォルト', () => <ListItem>デフォルト</ListItem>);
