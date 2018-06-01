9e65256e11a780bd396769413feaa62b3fb98ddb
clomaru <k.cloudspider@gmail.com>
2018-06-01 20:51:36 +0900

    [Add]ぐちゃぐやではあるがstorybookが動いた

--- src/components/atoms/Heading/index.stories.tsx
+++ src/components/atoms/Heading/index.stories.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
 import Heading from './index.js';
 
-export default stories =>
-	stories.add('デフォルト', () => <Heading type="h2">見出し</Heading>);
+// export default stories =>
+// 	stories.add('デフォルト', () => <Heading type="h2">見出し</Heading>);
+
+storiesOf('Heading', module)
+	.add('h1', () => <Heading type="h1">見出し</Heading>)
+	.add('h2', () => <Heading type="h2">見出し</Heading>)
+	.add('h3', () => <Heading type="h3">見出し</Heading>);
