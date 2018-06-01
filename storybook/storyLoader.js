ee298e7ce40cd8a1207926d99fdb4ef184f34e64
clomaru <k.cloudspider@gmail.com>
2018-06-02 01:31:26 +0900

    [Fix]gitignore

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -1,16 +1,24 @@
+
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
 function loadStories() {
-	require('../lib/components/atoms/Button/index.stories');
-	require('../lib/components/atoms/Heading/index.stories');
-	require('../lib/components/atoms/ListItem/index.stories');
+  require('../lib/components/atoms/Button/index.stories');
+  require('../lib/components/atoms/Heading/index.stories');
+  require('../lib/components/atoms/ListItem/index.stories');
+  
 }
 
 const stories = [
-	'../lib/components/atoms/Button/index.stories',
-	'../lib/components/atoms/Heading/index.stories',
-	'../lib/components/atoms/ListItem/index.stories'
+  '../lib/components/atoms/Button/index.stories',
+  '../lib/components/atoms/Heading/index.stories',
+  '../lib/components/atoms/ListItem/index.stories',
+  
 ];
 
 module.exports = {
-	loadStories,
-	stories
+  loadStories,
+  stories,
 };
