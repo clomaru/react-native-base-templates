bb853f3b41168fac54b9f575185691db9143eb81
clomaru <k.cloudspider@gmail.com>
2018-06-04 22:21:25 +0900

    [Clean]

--- src/components/atoms/Heading/index.stories.tsx
+++ src/components/atoms/Heading/index.stories.tsx
@@ -1,20 +1,17 @@
 import * as React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import Heading from './index.js';
 
-storiesOf('Heading', module)
-	.add('heading', () => (
-		<View>
-			<Heading type="h1">見出し1</Heading>
-			<Heading type="h2">見出し2</Heading>
-			<Heading type="h3">見出し3</Heading>
-			<Heading type="h4">見出し4</Heading>
-			<Heading type="h5">見出し5</Heading>
-			<Heading type="h6">見出し6</Heading>
-		</View>
-
-
-	)
+storiesOf('Heading', module).add('heading', () => (
+	<View>
+		<Heading type="h1">見出し1</Heading>
+		<Heading type="h2">見出し2</Heading>
+		<Heading type="h3">見出し3</Heading>
+		<Heading type="h4">見出し4</Heading>
+		<Heading type="h5">見出し5</Heading>
+		<Heading type="h6">見出し6</Heading>
+	</View>
+));
