1dda75322560892e4c4b419ee6e1a5cad9937d4b
clomaru <k.cloudspider@gmail.com>
2018-06-11 22:01:46 +0900

    [Add]add FlatList

--- src/containers/pages/main.tsx
+++ src/containers/pages/main.tsx
@@ -1,26 +1,43 @@
 import * as React from 'react';
 import styled from 'styled-components/native';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 interface Props {}
 
 interface State {}
 
 export default class Main extends React.Component<Props, State> {
+	state = {
+		items: []
+	};
+	page = 1;
 	public render() {
 		return (
 			<Container>
-				<TouchableOpacity onPress={() => this.onPressFetch()}>
+				<TouchableOpacity onPress={() => this.fetchRepositories()}>
 					<Text>Fetch</Text>
 				</TouchableOpacity>
+				<FlatList
+					data={this.state.items}
+					renderItem={({ item }) => (
+						<Text style={{ padding: 20 }}>{item.name}</Text>
+					)}
+					keyExtractor={item => item.id}
+					onEndReached={() => this.fetchRepositories()}
+					onEndReachedThreshold={0.1}
+				/>
 			</Container>
 		);
 	}
 
-	onPressFetch() {
-		fetch('https://api.github.com/search/repositories?q=react')
+	fetchRepositories() {
+		const newPage = this.page + 1;
+		fetch(`https://api.github.com/search/repositories?q=react&page=${newPage}`)
 			.then(response => response.json())
-			.then(({ items }) => console.log(items));
+			.then(({ items }) => {
+				this.page = newPage;
+				this.setState({ items: [...this.state.items, ...items] });
+			});
 	}
 }
 
