e60ec59ef70349c662397718d584bfe024b66599
clomaru <k.cloudspider@gmail.com>
2018-06-12 22:26:10 +0900

    [backup]

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,39 @@
 import * as React from 'react';
 import { Navigation } from 'react-native-navigation';
 import registerScreens from './screen';
-import Icon from 'react-native-vector-icons/FontAwesome';
-// import * as appActions from './actions/index';
-// import { Provider } from 'react-redux';
-// import configureStore from './store';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+// import * as actions from './actions/index';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 
-// const store = configureStore();
+const store = configureStore();
+registerScreens(store, Provider);
 
-// TODO: app.tsxにproviderを書く
-// TODO: まず最初にapp.tsxのclass化？無理ならredux導入してみてもいいかも
-// TODO: その後mainのtodoやる
+// ///////////////
+// // dont work
+// ///////////////
+// export default class App {
+// 	private startApp(): any {
+// 		Navigation.startTabBasedApp({
+// 			tabs: [
+// 				{
+// 					label: 'One',
+// 					screen: 'searchRepository.Main',
+// 					title: 'Screen One'
+// 				},
+// 				{
+// 					label: 'Two',
+// 					screen: 'searchRepository.Main2',
+// 					title: 'Screen Two'
+// 				}
+// 			]
+// 		});
+// 	}
+// }
 
-registerScreens();
-
+///////////////
+// work
+///////////////
 Navigation.startTabBasedApp({
 	tabs: [
 		{
@@ -28,30 +48,3 @@ Navigation.startTabBasedApp({
 		}
 	]
 });
-
-// Navigation.startSingleScreenApp({
-// 	screen: {
-// 		screen: 'searchRepository.Main',
-// 		title: 'Hello World',
-// 		navigatorStyle: {},
-// 		navigatorButtons: {}
-// 	}
-// });
-
-// export default class App extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.startApp();
-// 	}
-//
-// 	startApp() {
-// 		Navigation.startSingleScreenApp({
-// 			screen: {
-// 				screen: 'searchRepository.Main',
-// 				title: 'Hello World',
-// 				navigatorStyle: {},
-// 				navigatorButtons: {}
-// 			}
-// 		});
-// 	}
-// }
--- src/screen.tsx
+++ src/screen.tsx
@@ -3,9 +3,19 @@ import { Navigation } from 'react-native-navigation';
 import Main from './containers/pages/main';
 import Main2 from './containers/pages/main2';
 
-export default function registerScreens() {
-	Navigation.registerComponent('searchRepository.Main', () => Main);
-	Navigation.registerComponent('searchRepository.Main2', () => Main2);
+export default function registerScreens(store: any, Provider: any) {
+	Navigation.registerComponent(
+		'searchRepository.Main',
+		() => Main,
+		store,
+		Provider
+	);
+	Navigation.registerComponent(
+		'searchRepository.Main2',
+		() => Main2,
+		store,
+		Provider
+	);
 }
 
 // export default (store: any, Provider: any) => {
