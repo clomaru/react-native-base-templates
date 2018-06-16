0f44d7ba576d4bc66f188d1c8cad91d39f101f8d
clomaru <k.cloudspider@gmail.com>
2018-06-16 20:31:01 +0900

    [Add]add anchor

---
+++ src/components/atoms/Txt/index.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import styled from 'styled-components/native';
+import styledComponents from 'styled-components';
+import styledComponentsTS from 'styled-components-ts';
+import { Text } from 'react-native';
+
+enum TagSize {
+	s,
+	m,
+	l
+}
+
+interface Props {
+	children?: string;
+	size?: TagSize;
+}
+
+const Txt: React.SFC<Props> = ({ children, size = 'm', ...props }) => (
+	<Wrapper size={size} {...props}>
+		{children}
+	</Wrapper>
+);
+export default Txt;
+
+export const Anchor: React.SFC<Props> = ({ children, ...props }) => (
+	<StyledTxt {...props}>{children}</StyledTxt>
+);
+
+const Wrapper = styledComponentsTS<StyledProps>(styledComponents(Text))`
+	font-size: ${p => tagSize[p.size]};
+`;
+
+const StyledTxt = styled(Txt)`
+	color: blue;
+`;
+
+const tagSize = {
+	s: '10px',
+	m: '20px',
+	l: '30px'
+};
