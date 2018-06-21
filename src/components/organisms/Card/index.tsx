fca848a2d9309eb145f9433ed7acf88b9b9135ab
mrsekut <k.cloudspider@gmail.com>
2018-06-22 02:48:22 +0900

    [Add]add organisms/Card

---
+++ src/components/organisms/Card/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import styled from 'styled-components/native';
+import styledComponents from 'styled-components';
+import styledComponentsTS from 'styled-components-ts';
+import { Text, View,, Image } from 'react-native';
+import Txt from '../../atoms/Txt/index';
+
+interface Props {
+}
+
+const Card: React.SFC<Props> = ({ ...props }) => (
+	<Wrapper>
+		<Text>{props.name}</Text>
+		<Image
+			style={{ width: 100, height: 100 }}
+			source={{ url:props.owner.avatar_url }}
+		/>
+		<Text >{props.owner.login}</Text>
+		<Text>star: {props.stargazers_count}</Text>
+		<Text>url: {props.html_url}</Text>
+		<Text>Description: {props.description}</Text>
+	</Wrapper>
+);
+export default Card;
+
+const Wrapper = styled(View)`
+`
+
+
+// flex: 1,
+// justifyContent: 'center',
+// alignItems: 'center',
+// backgroundColor: '#F5FCFF'
+// fontSize: 20,
+// textAlign: 'center',
+// margin: 10
