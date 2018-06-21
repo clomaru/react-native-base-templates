189b811238e1d7f298b7622d71fb8ac47898b231
mrsekut <k.cloudspider@gmail.com>
2018-06-21 23:44:25 +0900

    [Add]add molecules

---
+++ src/components/molecules/SubmittionBox/index.stories.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { storiesOf, module } from '@storybook/react-native';
+import SubmittionBox from './index.js';
+
+storiesOf('SubmittionBox', module).add('default', () => <SubmittionBox />);
---
+++ src/components/molecules/SubmittionBox/index.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import styled from 'styled-components/native';
+import styledComponents from 'styled-components';
+import styledComponentsTS from 'styled-components-ts';
+import Button from '../../atoms/Button/index';
+import TextBox from '../../atoms/TextBox/index';
+
+interface Props {
+	children?: string;
+	buttonText?: string;
+	onPress?: () => void;
+}
+
+const SubmittionBox: React.SFC<Props> = ({
+	children,
+	buttonText,
+	onPress,
+	...props
+}) => (
+	<Wrapper>
+		<TextBox {...props}>{children}</TextBox>
+		<StyleButton onPress={onPress}>{buttonText}</StyleButton>
+	</Wrapper>
+);
+export default SubmittionBox;
+
+interface StyledProps {
+	onPress?: () => void;
+}
+
+const Wrapper = styled.View``;
+
+const StyleButton = styledComponentsTS<StyledProps>(styledComponents(Button))`
+	margin-top: 10px;
+`;
