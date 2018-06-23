7c7e901c132d01fa48a6cb74c97d25dbf2a35b02
mrsekut <k.cloudspider@gmail.com>
2018-06-23 23:56:21 +0900

    [Add]add react-native-debugger

--- src/store.tsx
+++ src/store.tsx
@@ -10,7 +10,8 @@ export const configureStore = () => {
 	return createStore(
 		combineReducers({
 			reducer
-		})
+		}),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	);
 };
 export default configureStore;
