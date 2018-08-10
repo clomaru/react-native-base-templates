be1973bab5351c241d7e93e011ca9ed6c65d7cb3
mrsekut <k.cloudspider@gmail.com>
2018-08-10 17:56:31 +0900

    [Fix]DetailPage -> components

--- src/screen.tsx
+++ src/screen.tsx
@@ -3,7 +3,7 @@ import { Navigation } from "react-native-navigation";
 import SearchRepositoryPage from "./containers/pages/SearchRepositoryPage";
 import SagasPostSearchPage from "./containers/pages/SagasPostSearchPage";
 import RecomposeCounterPage from "./components/pages/RecomposeCounterPage";
-import DetailPage from "./containers/pages/DetailPage";
+import DetailPage from "./components/pages/DetailPage";
 
 export default function registerScreens(store: any, Provider: any) {
   Navigation.registerComponent(
